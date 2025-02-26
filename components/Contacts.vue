ba91cd7e7e3c1fbe9782b3ce91fb3b67a7b9d554
vin327 <zvereva-julia1979@mail.ru>
2025-02-26 17:44:57 +0300

    first commit

--- components/Contacts.vue
+++ components/Contacts.vue
@@ -35,7 +35,7 @@
             </div>
           </div>
           <div class="addres-maps">
-            <img src="../assets/images/addres-map.jpeg" alt="">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.392040242475!2d30.327103077461285!3d59.92563666314693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469631aa6e3b3193%3A0xe7c54da95407a617!2z0L3QsNCx0LXRgNC10LbQvdCw0Y8g0YDQtdC60Lgg0KTQvtC90YLQsNC90LrQuCwgOTDQujEsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTExODA!5e0!3m2!1sru!2sru!4v1740580800401!5m2!1sru!2sru" width="856" height="656" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
